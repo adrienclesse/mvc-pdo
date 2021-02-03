de9d6e422f24df060e683c6c0ee9c6b108d45c5a
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2021-02-03 16:36:50 +0100

    add styling to homepage

--- view/includes/header.php
+++ view/includes/header.php
@@ -5,41 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"/>
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <link href="view/includes/styling/css/style.css" rel="stylesheet" type="text/css">
     <title>Becode - Boiler plate MVC</title>
 </head>
 <body>
-<header>
-    
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" href="index.php?page=home">Fantalang</a>
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-    <li class="nav-item active">
-        <a class="nav-link" href="index.php?page=home">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php?page=info">About us <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php?page=login">Log in</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="index.php?page=register">Sign up</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-</header>
+    <div class="wave-container-purple">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark m-2 bg-purple-main">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01">
+                <a class="navbar-brand mx-3 px-3" href="index.php?page=home">Fantalang</a>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mx-3 px-3">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php?page=home">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php?page=info">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?page=login">Log in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="index.php?page=register">Sign up</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
--- view/welcome.php
+++ view/welcome.php
@@ -1,10 +1,11 @@
 <?php require 'includes/header.php'?>
-<div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome on your personal space.</h1>
-        <p>Here you can take a test to evaluate your level by clicking the button or meet people to learn your language with.</p>
-    </div>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
+        <div class="page-header">
+            <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome on your personal space.</h1>
+            <p>Here you can take a test to evaluate your level by clicking the button or meet people to learn your language with.</p>
+        </div>
+        <p>
+            <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+            <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+        </p>
+
 <?php require 'includes/footer.php'?>
