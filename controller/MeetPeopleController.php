8281cc680aea4a9dd7eb9d44c4ef9f658ca5fe09
WillemDT369 <detroyerwillem@hotmail.com>
2021-02-04 11:16:58 +0100

    Add query to meetpeople

--- controller/MeetPeopleController.php
+++ controller/MeetPeopleController.php
@@ -8,8 +8,19 @@ class MeetPeople
     {
         //you should not echo anything inside your controller - only assign vars here
         // then the view will actually display them.
+        require "config.php";
+        $conn = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+        $sql = "SELECT * FROM `users` WHERE id=1;";
+        $query = $conn->prepare($sql);
+        $query->execute();
+ 
+        while($fetch = $query->fetch()){
+
+        echo $fetch['created_at'];
+        }
         
         //load the view
         require 'view/meetpeople.php';
+            
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 //include all your model files here
 require 'Model/User.php';
 //include all your controllers here
